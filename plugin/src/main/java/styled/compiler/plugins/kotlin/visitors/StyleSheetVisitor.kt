4dfd71e875d6f99c1a0c1f9b16470ce67e093446
tretikoff <Konstantin.Tretiakov@jetbrains.com>
2021-12-11 11:08:01 +0300

    More examples

--- plugin/src/main/java/styled/compiler/plugins/kotlin/visitors/StyleSheetVisitor.kt
+++ plugin/src/main/java/styled/compiler/plugins/kotlin/visitors/StyleSheetVisitor.kt
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrProperty
 import org.jetbrains.kotlin.ir.declarations.IrValueParameter
 import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.IrConst
 import org.jetbrains.kotlin.ir.expressions.IrDelegatingConstructorCall
 import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
 import org.jetbrains.kotlin.ir.util.getArgumentsWithIr
@@ -19,11 +20,14 @@ class StyleSheetVisitor(private var name: String) : IrElementVisitor<Unit, Strin
     override fun visitElement(element: IrElement, data: StringBuilder) {
         when (element) {
             is IrDelegatingConstructorCall -> {
-                arguments = element.getArgumentsWithIr()
-                    // Skipping non-constant values as don't know how to evaluate them right now
-                    .filterIsInstanceMapNotNull<Pair<IrValueParameter, IrConstImpl<*>>, Parameter> { (parameter, expr) ->
-                        parameter.name.asString() to expr.value
-                    }.toMap().also { name = it["name"]?.toString() ?: name }
+                arguments = // Skipping non-constant values as don't know how to evaluate them right now
+                    element.getArgumentsWithIr()
+                        // Skipping non-constant values as don't know how to evaluate them right now
+                        .mapNotNull { (parameter, expr) ->
+                            (expr as? IrConst<*>)?.let {e ->
+                                parameter.name.asString() to e.value
+                            }
+                        }.toMap().also { name = it["name"]?.toString() ?: name }
             }
             is IrProperty -> {
                 val propName = element.name.asString()
--- src/main/kotlin/WelcomeStyles.kt
+++ src/main/kotlin/WelcomeStyles.kt
@@ -4,6 +4,7 @@ import styled.StyleSheet
 object WelcomeStyles : StyleSheet("WelcomeStyles", isStatic = true) {
     val textContainer by css {
         padding(5.px)
+        grow(Grow.GROW_SHRINK)
 
         backgroundColor = rgb(8, 97, 22)
         color = rgb(56, 246, 137)
@@ -12,12 +13,14 @@ object WelcomeStyles : StyleSheet("WelcomeStyles", isStatic = true) {
     val textInput by css {
         margin(vertical = 5.px)
         padding(15.px, 18.px)
-
+        fontWeight = FontWeight.w500
         fontSize = 2.unit
     }
     val textProperty by css {
         alignContent = Align.end
     }
 }
+
+object Fonts : StyleSheet("Fonts", WelcomeStyles)
 
 val Int.unit get() = (this * 8).px
